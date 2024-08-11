c7c2752d5cc245275ec274f6744401d6f9270e75
fahadhussain3 <fahad.hussain3133@gmail.com>
2024-08-11 13:49:58 +0500

    Add footer with dynamic copyright year

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="bg-black py-2" style="text-align: center; color: white;">
+    <?php
+        $date=date("y");
+        echo "Copyrights-".$date." | All rights reserved";
+    ?>
+    </div>
+</body>
+</html>
